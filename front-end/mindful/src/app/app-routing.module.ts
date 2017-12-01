8b73fe50e1c58fa2f4b4065d459108760f169f7f
nataliefrancis <natdfrancis@gmail.com>
2017-12-01 16:01:18 -0700

    adds a timer

--- front-end/mindful/src/app/app-routing.module.ts
+++ front-end/mindful/src/app/app-routing.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { CommunityComponent } from './community/community.component';
+import { LandingComponent } from './landing/landing.component'
 
 const routes: Routes = [
+	{
+	    path: '',
+	    component: LandingComponent
+	},
     {
         path: 'community',
         component: CommunityComponent
--- front-end/mindful/src/app/landing/landing.component.ts
+++ front-end/mindful/src/app/landing/landing.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs/Rx';
+
 
 @Component({
   selector: 'app-landing',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+
+	ticks = 0;
+
+	minutesDisplay: number = 0;
+	secondsDisplay: number = 0;
+
+	sub: Subscription;
+
+    constructor() { }
+
+      ngOnInit() {
+      }
+      		//Activates the timer from the click function in the html
+		private startTimer() {
+
+		  let timer = Observable.timer(1, 1000);
+		  this.sub = timer.subscribe(
+		      t => {
+		          this.ticks = t;
+		          
+		          this.secondsDisplay = this.getSeconds(this.ticks);
+		          this.minutesDisplay = this.getMinutes(this.ticks);
+		      }
+		  );
+		}
+
+		private getSeconds(ticks: number) {
+		  return this.pad(ticks % 60);
+		}
+
+		private getMinutes(ticks: number) {
+		   return this.pad((Math.floor(ticks / 60)) % 60);
+		}
+
+		private pad(digit: any) { 
+		  return digit <= 9 ? '0' + digit : digit;
+		}
+
 
 }
