b330df4067b541a6565831d5fa62ed2be6de1caa
nataliefrancis <natdfrancis@gmail.com>
2017-12-01 08:54:27 -0700

    continues work on form and having data populate

--- front-end/mindful/src/app/app.module.ts
+++ front-end/mindful/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AgmCoreModule } from '@agm/core';
+import { FormsModule } from '@angular/forms';
 
 import { environment } from '../environments/environment';
 
@@ -26,6 +27,7 @@ export const firebaseConfig = environment.firebaseConfig;
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AngularFireModule.initializeApp(firebaseConfig),
     AngularFireDatabaseModule,
     AngularFirestoreModule,
--- front-end/mindful/src/app/community/community.component.ts
+++ front-end/mindful/src/app/community/community.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-community',
   templateUrl: './community.component.html',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommunityComponent implements OnInit {
 
+	public myDate: Date;
+	details;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  // makes the date input the correct format
+  public onInput(value: Date): void{
+      this.myDate = value;
+  }
+
+  //retrives the details of the form
+  onSubmit = (formData) => {
+  	this.details=formData;
   }
 
 }
